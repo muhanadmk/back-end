a0ae568cf8ae7e3492fa1c8596098d3174fbb069
muhanad <mouhandmk1@gmail.com>
2021-06-12 11:04:44 +0200

    install  body-parser

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.post('/api/stuff', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: 'Objet créé !'
+  });
+});
 
 app.use((req, res, next) => {
   console.log('Requête reçue !');
