3efefa885a54aa0363c2e798666d587c121dcadf
muhanad <mouhandmk1@gmail.com>
2021-06-12 13:35:49 +0200

    ajouter de token pour le logoin

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-// app.use('/api/sauces', stuffRoutes);
+app.use('/api/sauces', stuffRoutes);
 app.use('/api/auth', userRoutes);
 
 module.exports = app;
--- controllers/stuff.js
+++ controllers/stuff.js
@@ -1,27 +1,30 @@
-// const Thing = require('../models/Thing');
+const Thing = require('../models/Thing');
 
-// exports.createThing = (req, res, next) => {
-//   const thing = new Thing({
-//     title: req.body.title,
-//     description: req.body.description,
-//     imageUrl: req.body.imageUrl,
-//     price: req.body.price,
-//     userId: req.body.userId
-//   });
-//   thing.save().then(
-//     () => {
-//       res.status(201).json({
-//         message: 'Post saved successfully!'
-//       });
-//     }
-//   ).catch(
-//     (error) => {
-//       res.status(400).json({
-//         error: error
-//       });
-//     }
-//   );
-// };
+exports.createThing = (req, res, next) => {
+  const thing = new Thing({
+    id : req.body.id,
+    userId: req.body.userId,
+    name: req.body.name,
+    manufacturer : req.body.manufacturer,
+    mainPepper : req.body.mainPepper,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    heat: req.body.heat,
+  });
+  thing.save().then(
+    () => {
+      res.status(201).json({
+        message: 'Post saved successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+};
 
 // exports.getOneThing = (req, res, next) => {
 //   Thing.findOne({
